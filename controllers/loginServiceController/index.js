dd77f0945ca3d2eccc090867e08e6202c8cd6f2a
DandunAdi <dandun.adi@gmail.com>
2023-11-30 22:45:20 +0700

    update header corporatelogin

--- controllers/loginServiceController/index.js
+++ controllers/loginServiceController/index.js
@@ -5,6 +5,8 @@ const router = express.Router();
 /* ----------------------------- CORPORATE LOGIN ---------------------------- */
 router.post("/corporatelogin", (req, res) => {
   res.set("access_token", "d35cece3-9745-4e2b-9c41-59b735238cac");
+  res.set("content-type", "application/json;charset=UTF-8");
+  res.set("expire", "900");
   res.json(data.corporateLogin);
 });
 
