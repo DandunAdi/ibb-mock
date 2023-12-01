8f0c038bdc3f7197304ec34385f465feaf75d53f
DandunAdi <dandun.adi@gmail.com>
2023-12-01 16:09:41 +0700

    add saveListPayroll

--- controllers/massTransferServiceController/index.js
+++ controllers/massTransferServiceController/index.js
@@ -7,8 +7,14 @@ router.post("/deletingmasstransfer", (req, res) => {
   res.json(data.deletingMassTransfer);
 });
 
+/* ----------------------------- INQUIRY ACCOUNT ---------------------------- */
 router.post("/inquiry/account", (req, res) => {
   res.json(data.inquiryAccount);
+});
+
+/* ---------------------------- SAVE LIST ACCOUNT --------------------------- */
+router.post("/savelistpayroll", (req, res) => {
+  res.json(data.saveListPayroll);
 });
 
 module.exports = router;
